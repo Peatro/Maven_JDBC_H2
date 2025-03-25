c136940fad0a463c2c0759918db99d1a0644aa8a
Peatro <PeatroXD@yandex.ru>
2025-03-25 17:07:49 +0300

    Добавлен класс конфигурации БД

---
+++ src/main/java/org/example/config/DatabaseConfig.java
@@ -0,0 +1,15 @@
+package org.example.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfig {
+    private static final String JDBC_URL = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1";
+    private static final String USER = "testUser";
+    private static final String PASSWORD = "";
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+    }
+}
