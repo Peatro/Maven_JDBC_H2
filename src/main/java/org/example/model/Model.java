790d7072249b1de631a19fc05e31e2abc46230d1
Peatro <PeatroXD@yandex.ru>
2025-03-25 17:04:29 +0300

    Добавлен интерфейс и один класс хранимой в БД сущности

---
+++ src/main/java/org/example/model/Model.java
@@ -0,0 +1,15 @@
+package org.example.model;
+
+public interface Model {
+    int getId();
+
+    void setId(int id);
+
+    String getName();
+
+    void setName(String name);
+
+    String getEmail();
+
+    void setEmail(String email);
+}
